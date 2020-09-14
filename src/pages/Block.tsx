10f83d0175bc1a8d6d6027825067d1af19ed667f
Alexander Shevtsov <randomlogin76@gmail.com>
2020-09-14 16:55:16 +0300

    populated-fields-for-block

--- src/pages/Block.tsx
+++ src/pages/Block.tsx
@@ -16,6 +16,9 @@ const Block: React.FC<Props> = ({ height }) => {
     return <div>Not Found</div>
   }
   const { block, maxHeight } = state;
+  console.log(block)
+  let blockDateTime = new Date(block.time*1000)
+  console.log(blockDateTime)
   return <div styleName="block">
     <div styleName="header">
       { block.height !== 0 ? <a href="#"></a> : "" }
@@ -23,6 +26,25 @@ const Block: React.FC<Props> = ({ height }) => {
       { block.height !== maxHeight  ? <a href="#"></a> : "" }
     </div>
     <div><b>Hash:</b> {block.hash}</div>
+    <div><b>Weight:</b> {block.weight}</div>
+    <div><b>Time:</b> {block.time}</div>
+    <div>Technical info:
+      <li>size:{block.size}</li>
+      <li>version:{block.version}</li>
+      <li>hashMerkleRoot:{block.hashMerkleRoot}</li>
+      <li>witnessRoot:{block.witnessRoot}</li>
+      <li>treeRoot:{block.treeRoot}</li>
+      <li>reservedRoot:{block.reservedRoot}</li>
+      <li>mask:{block.mask}</li>
+      <li>time:{blockDateTime.toLocaleString('en-GB')}</li>
+      <li>bits:{block.bits}</li>
+      <li>difficulty:{block.difficulty}</li>
+      <li>chainwork:{block.chainwork}</li>
+      <li>nonce:{block.nonce}</li>
+      <li>extraNonce:{block.extraNonce}</li>
+    </div>
   </div>
+
+
 };
 export default Block;
